418104b6af5f410dac5d10d0cf22605dcb51f347
hasibur14 <hasib141415@gmail.com>
2025-07-31 22:36:48 +0600

    basic redux

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2>BASIC REDUX</h2>
+      <button>Increment</button>
+      <button>0</button>
+      <button>Descriment</button>
+    </div>
+  );
+}
+
+export default App;
