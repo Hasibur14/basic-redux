bf51584be546cef9e0a11e47bc7f91507a113033
hasibur14 <hasib141415@gmail.com>
2025-08-21 11:48:04 +0600

    [ADD] implement the counter slider function with useDispath hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { useDispatch } from "react-redux";
 import "./App.css";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <div>
       <h2>BASIC REDUX</h2>
-      <button>Increment</button>
+      <button onClick={handleIncrement}>Increment</button>
       <button>0</button>
-      <button>Descriment</button>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
 }
